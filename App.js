e20acf28ecccbccb3ef8d9cb21073b59843080e0
Praful Ghadge <PrafulG@CCMT.local>
2018-03-13 16:54:48 +0530

    add the react native code

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Text } from 'react-native';
-import { Provider } from "react-redux";
-// import App from './App';
-import store from "./store";
-import ContactForm from './components/ContactForm';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const handleSubmit = values => {    
-    alert(`submitting form with values123 = ${values}`);
-};
+Amplify.configure({
+    Auth: {
+        identityPoolId: 'arn:aws:cognito-idp:us-east-1:140109379489:userpool/us-east-1_ebxdGuzZF', //REQUIRED - Amazon Cognito Identity Pool ID
+        region: 'US East (Virginia)', // REQUIRED - Amazon Cognito Region
+        userPoolId: 'us-east-1_ebxdGuzZF', //OPTIONAL - Amazon Cognito User Pool ID
+        userPoolWebClientId: '344vf234u1okuch0u7jqulgte3', //OPTIONAL - Amazon Cognito Web Client ID
+    },
+    Analytics: {
+      appId: '344vf234u1okuch0u7jqulgte3', //OPTIONAL -  Amazon Pinpoint App ID
+      region: 'US East (Virginia)', //OPTIONAL -  Amazon service region
+  }
+  
+ });
 
-export default class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <ContactForm handleSubmit={handleSubmit}/>
-            </Provider>
-        );
-    }
-};
-AppRegistry.registerComponent('tutorialProject', () => App);
+// const AppWithAuth = withAuthenticator(App);
+
+// const federated = {
+//     google_client_id: '931888698125-affefnf2d3gl8qgs3qo18ec18rdv46ul.apps.googleusercontent.com',
+// };
+
+// ReactDOM.render(<AppWithAuth federated={federated}/>, document.getElementById('root'))
+
+
+
+
+ class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default withAuthenticator(App);
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { StyleSheet, View, Text, TouchableOpacity, TextInput } from 'react-native';
-
-const renderField = ({ label, keyboardType, name }) => {
-    return (<View style={{ flexDirection: 'row', height: 50, alignItems: 'center' }}>
-        <Text style={{ fontSize: 14, fontWeight: 'bold', width: 80 }}>{label}</Text>
-        <TextInput style={{ borderColor: 'steelblue', borderWidth: 1, height: 37, width: 220, padding: 5 }}
-            keyboardType={keyboardType}
-        >
-        </TextInput>
+//Validation
+const validate = values => {
+    const errors = {};      
+    if (!values.username) {
+        errors.username = 'Required'
+    } else if (values.username.length > 20) {
+        errors.username = 'username must be less than or equal 20 characters'
+    } 
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    } 
+    if (!values.age) {
+        errors.age = 'Required'
+    } else if (isNaN(Number(values.age))) {
+        errors.age = 'Age must be a number'
+    } else if (Number(values.age) < 18) {
+        errors.age = 'You must be at least 18 years old'
+    }
+    return errors
+}
+const warn = values => {
+    const warnings = {};  
+    if (values.age < 10) {
+        warnings.age = 'You seem a bit young...'
+    }  
+    return warnings
+}
+const renderField = ({ label, keyboardType, meta: { touched, error, warning }, input: { onChange, ...restInput }}) => {
+    return (<View style={{ flexDirection: 'column', height: 70, alignItems: 'flex-start' }}>
+        <View style={{ flexDirection: 'row', height: 50, alignItems: 'center' }}>
+            <Text style={{ fontSize: 14, fontWeight: 'bold', width: 80 }}>{label}</Text>
+            <TextInput style={{ borderColor: 'steelblue', borderWidth: 1, height: 37, width: 220, padding: 5 }}
+                keyboardType={keyboardType} onChangeText={onChange} {...restInput}
+            >
+            </TextInput>
+        </View>
+        {touched && ((error && <Text style={{ color: 'red' }}>{error}</Text>) ||
+                (warning && <Text style={{ color: 'orange' }}>{warning}</Text>))}
     </View>);
 };
+const submit = values => {
+    alert(`Validation success. Values = ~${JSON.stringify(values)}`);    
+}
 const ContactComponent = props => {
     const { handleSubmit } = props;
     return (
         <View style={{ flex: 1, flexDirection: 'column', margin: 40, justifyContent: 'flex-start', }}>
             <Text style={{ fontSize: 18, fontWeight: 'bold', width: 200, textAlign: 'center', margin: 10 }}>Contact Form</Text>
-            <Field keyboardType="default" label="Username: " component={renderField} name="Username" />
-            <Field keyboardType="email-address" label="Email: " component={renderField} name="Email" />
-            <TouchableOpacity onPress={handleSubmit} style={{ margin: 10, alignItems: 'center' }}>
+            <Field name="username" keyboardType="default" label="First Name: " component={renderField} />
+            <Field name="username" keyboardType="default" label="Last Name: " component={renderField} />
+            <Field name="email" keyboardType="email-address" label="Email: " component={renderField} />
+            <Field name="age" keyboardType="numeric" label="Phone: " component={renderField} />
+            <TouchableOpacity onPress={handleSubmit(submit)} style={{ margin: 10, alignItems: 'center' }}>
                 <Text style={{
                     backgroundColor: 'steelblue', color: 'white', fontSize: 16,
                     height: 37, width: 200, textAlign: 'center', padding: 10
@@ -28,7 +65,9 @@ const ContactComponent = props => {
     );
 }
 const ContactForm = reduxForm({
-    form: 'contact', // a unique identifier for this form
+    form: 'contact', // a unique identifier for this form    
+    validate,                // <--- validation function given to redux-form    
+    warn
 })(ContactComponent);
 
 export default ContactForm;
